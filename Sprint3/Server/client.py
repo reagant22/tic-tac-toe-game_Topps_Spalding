db3942912fbdee8f7731f298f1aed248c8bcc383
shea <shea1@colostate.edu>
2024-11-03 21:04:58 -0700

    Changes to fix issues

--- Sprint3/Server/client.py
+++ Sprint3/Server/client.py
@@ -10,6 +10,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 print(f"Connected to the game server!")
 
+# Receive the player's symbol from the server
+player_symbol = pickle.loads(s.recv(1024))  
+print(f"You are Player {player_symbol}") 
+
 player_symbol = "X" if s.getsockname()[1] == 12783 else "O"  # Determine symbol based on socket port
 game = TicTacToe(player_symbol)
 
@@ -18,19 +22,20 @@ while True:
     game.symbol_list = pickle.loads(s.recv(1024))
     game.draw_grid()
 
-    # Player's turn to make a move
-    print(f"Your turn! Enter coordinate (e.g., 1A): ")
-    move = input()
-    
-    # Send the move to the server
-    move = pickle.dumps(move)
-    s.send(move)
-
-    # Wait for the server to notify the end of the game
-    response = s.recv(1024)
-    response = pickle.loads(response)
-    if response == "Game Over":
-        print("Game Over! Thanks for playing!")
+    # Check if the game is over
+    if isinstance(game.symbol_list, str):
+        print(game.symbol_list)
         break
+
+    # player turn to make a move
+    if player_symbol == game.player_symbol:
+        print(f"Your turn! Enter coordinate (e.g., 1A): ")
+        move = input()
+    
+    # Send move to the server
+        move = pickle.dumps(move)
+        s.send(move)
+    else:
+        print("Waiting for the other player's move...")  # CHANGE: Inform player it's the other player's turn
 
 s.close()
--- Sprint3/Server/server.py
+++ Sprint3/Server/server.py
@@ -12,19 +12,23 @@ s.listen(2)  # Listen for 2 clients
 print("Waiting for players to connect...")
 
 clients = []
+symbols = ["X", "O"]
 game = TicTacToe("X")  # Initialize the game
 
 # Accept connections from both clients
-for _ in range(2):
+for i in range(2):
     client_socket, client_address = s.accept()
-    clients.append(client_socket)
-    print(f"Connected to {client_address}!")
+    # clients.append(client_socket)
+    clients.append((client_socket, symbols[i]))  
+    client_socket.send(pickle.dumps(symbols[i])) 
+    print(f"Connected to {client_address} as Player {symbols[i]}!")
+
 
 # Main game loop
-while True:
-    for i in range(2):  # Loop over each client
-        client_socket = clients[i]
-        other_client_socket = clients[1 - i]  # Get the other client
+game_over = False
+while not game_over:
+    for i, (client_socket, player_symbol) in enumerate(clients):
+        other_client_socket, _ = clients[1 - i]
 
         # Send the current game state to the current player
         client_socket.send(pickle.dumps(game.symbol_list))
@@ -36,14 +40,23 @@ while True:
         # Process the move
         if game.edit_square(move):
             # If move is valid, send the updated game state to the other client
+            game.player_symbol = player_symbol
             other_client_socket.send(pickle.dumps(game.symbol_list))
 
             # Check for win or draw
-            if game.did_win(game.player_symbol) or game.is_draw():
-                # Notify both players of the end of the game
-                for cs in clients:
-                    cs.send(pickle.dumps("Game Over"))
-                break
+            if game.did_win(player_symbol):  # CHANGE: Check win condition for the current player
+                message = f"Player {player_symbol} wins!"  # CHANGE: Set win message
+                game_over = True  
+            elif game.is_draw():  
+                message = "It's a draw!"  
+                game_over = True  
+            else:
+                continue  # move to the next turn if no win/draw
+
+            # Notify both players of the end of the game
+            for cs, _ in clients:
+                cs.send(pickle.dumps(message))  # game-over message to all clients
+            break
 
 # Close sockets after the game
 for cs in clients:
